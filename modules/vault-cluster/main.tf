e0d605a9956bf1e281406c344b71c25ef33fb8d7
Dale Peakall <dale.peakall.1@lexisnexis.co.uk>
2018-06-04 12:53:02 +0000

    Modified to support rolling update.

--- modules/vault-cluster/main.tf
+++ modules/vault-cluster/main.tf
@@ -11,7 +11,9 @@ terraform {
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "aws_autoscaling_group" "autoscaling_group" {
-  name_prefix = "${var.cluster_name}"
+  lifecycle { create_before_destroy = true }
+
+  name = "${var.cluster_name} - ${aws_launch_configuration.launch_configuration.name}"
 
   launch_configuration = "${aws_launch_configuration.launch_configuration.name}"
 
